ce43436e1b742e3948165132b49c0fc4450e800c
Ian Muñoz Nuñez <ianemn@ian.kali>
2023-05-09 10:45:21 -0600

    PWM y lecturas de RPM del motor

--- pidEncoder.ino
+++ pidEncoder.ino
@@ -1,11 +1,17 @@
+int pot = 1; // Pin para lectura analogica del potenciometro (A1)
+float inPot; // Variable para el valor de la lectura del pin A1
+int outPWM = 6; // Pin para salida pwm al motor
+float rpm; // Contador para las rpm
+
 int pinA = 3; // Pin para interrupciones
-volatile int contador = 0; // Variable para conteo de pulsos
+volatile int cont = 0; // Variable para conteo de pulsos
 unsigned long previousMillis = 0; // Variable para tomar tiempo
-long interval = 1000; // Intervalo de tiempo para muestreo
+long interval = 100; // Intervalo de tiempo para muestreo
 
 void setup()
 {
     pinMode(pinA, INPUT); // Configuracion de pin 3 como entrada para interrupciones
+    pinMode(outPWM, OUTPUT); // Configuracion de salida del pin 6 para PWM
     Serial.begin(9600); // Comunicacion serial
     attachInterrupt(1, interrupcion, RISING); // Interrupciones en flanco de subida
 }
@@ -18,15 +24,22 @@ void loop()
     if((currentMillis - previousMillis) >= interval)
     {
         previousMillis = currentMillis; // Actualizacion de 'previousMillis'
-        Serial.print("Pulsos/seg: ");
-        Serial.println(contador);
-        contador = 0; // Pulsos a 0
+        rpm = 10*cont*(60.0/374.0); // RPM del eje principal
+        cont = 0; // Pulsos a 0
     }
+
+    inPot = analogRead(pot)*(100.0/1023.0) // Lectura del potenciometro de 0 - 100
+    analogWrite(outPWM, inPot*(255.0/100.0)); // PWM de 0 - 255
+
+    Serial.print("Lectura POT: ");
+    Serial.println(inPot);
+    Serial.print("RPM: ");
+    Serial.println(rpm);
 
 }
 
 void interrupcion()
 {
-    contador++; // Nuevo conteo de pulsos
+    cont++; // Nuevo conteo de pulsos
 }
 
